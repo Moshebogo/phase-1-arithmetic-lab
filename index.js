55050f70576dc8233740616deedba811b7e0ae63
Eli Moshe <moshebogo@gmail.com>
2023-01-08 03:18:03 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 7.75;
+let num2 = 8;
+let num3 = 16;
+let num4 = 6;
+
+const multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random()) +1 ;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 20);
